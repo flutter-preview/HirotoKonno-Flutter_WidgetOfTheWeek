b675f89fb531c6af90378f84d2b847fef3d1d1d5
HirotoKonno <great.hiroto@gmail.com>
2023-05-28 21:14:38 +0900

    implement SampleExpandedView

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# SafeArea
+# Expanded
 
 Youtube link\
-https://www.youtube.com/watch?v=lkF0TQJO0bA&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=147
+https://www.youtube.com/watch?v=_rnZaagadyo&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=144
 
-SampleSafeAreaView\
-<img width="820" alt="SafeArea" src="https://github.com/HirotoKonno/Flutter_WidgetOfTheWeek/assets/126723478/eb141e3e-06d0-4685-852f-8d684261d401">
+SampleExpandedView\
--- lib/main.dart
+++ lib/main.dart
@@ -14,31 +14,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: _SampleSafeAreaView(),
+      home: _SampleExpandedView(),
     );
   }
 }
 
-class _SampleSafeAreaView extends StatelessWidget {
+class _SampleExpandedView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
       body: SafeArea(
-        // top: false,
-        // bottom: false,
-        // minimum: EdgeInsets.all(10.0),
-        // maintainBottomViewPadding: true,
         child: Container(
-          // height: 2000,
-          color: Colors.black,
-          child: const Text(
-            "SafeArea SafeArea SafeArea SafeArea SafeArea SafeArea SafeArea SafeArea SafeArea",
-            style: TextStyle(
-              // overflow: TextOverflow.ellipsis,
-              color: Colors.white,
-              fontSize: 100,
-            ),
+          width: 500,
+          height: 500,
+          decoration: BoxDecoration(
+            color: Colors.orange[300],
+          ),
+          child: Row(
+            children: const [
+              Expanded(
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Card(
+                    color: Colors.green,
+                    child: Text(
+                      'Expanded(in Row)',
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                ),
+              ),
+              // Flexible(
+              //   child: SizedBox(
+              //     width: 100,
+              //     height: 100,
+              //     child: Card(
+              //       color: Colors.red,
+              //       child: Text('Flexible'),
+              //     ),
+              //   ),
+              // ),
+            ],
           ),
         ),
       ),
